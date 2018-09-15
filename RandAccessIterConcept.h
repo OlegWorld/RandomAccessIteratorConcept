e3b7d9a55ae836070dbbe88740c5455b03f67aa7
OlegWorld <osmironov@gmail.com>
2018-09-15 17:11:23 +0300

    добавил тесты и подчистил пойманные ошибки

--- RandAccessIterConcept.h
+++ RandAccessIterConcept.h
@@ -5,15 +5,15 @@
 #include <type_traits>
 #include <boost/concept_check.hpp>
 
-template <class T>
-struct MyDefaultConstructible {
-    BOOST_CONCEPT_USAGE(MyDefaultConstructible) {
-        T u1;
-        T u2{};
-        T();
-        T{};
-    }
-};
+//template <class T>
+//struct MyDefaultConstructible {
+//    BOOST_CONCEPT_USAGE(MyDefaultConstructible) {
+//        T u1;
+//        T u2{};
+//        T();
+//        T{};
+//    }
+//};
 
 template <class T>
 struct MyEqualityComparable {
@@ -31,7 +31,6 @@ template <class It, class = std::enable_if_t<std::is_copy_constructible<It>::val
                                              std::is_destructible<It>::value &&
                                              std::is_swappable<It>::value>>
 struct MyIterator {
-
     using value_type        = typename std::iterator_traits<It>::value_type;
     using difference_type   = typename std::iterator_traits<It>::difference_type;
     using reference         = typename std::iterator_traits<It>::reference;
@@ -62,8 +61,7 @@ struct MyInputIterator : public MyIterator<It>,
 
         (i != j) == !(i == j);
 
-        auto r = *i;
-        static_assert(std::is_same<decltype(r), reference>::value, "Type mismatch");
+        reference r = *i;
         static_assert(std::is_convertible<decltype(r), value_type>::value, "Can't be converted");
 
         It& it = ++i;
@@ -78,72 +76,143 @@ private:
     const It j;
 };
 
-template <class It, class = std::enable_if_t<std::is_default_constructible<It>::value>>
-struct MyForwardIterator : public MyInputIterator<It> {
+template <class It, class ValueT,
+          class = std::enable_if_t<std::is_class<It>::value || std::is_pointer<It>::value>>
+struct MyOutputIterator : public MyIterator<It> {
     using value_type        = typename std::iterator_traits<It>::value_type;
     using difference_type   = typename std::iterator_traits<It>::difference_type;
     using reference         = typename std::iterator_traits<It>::reference;
     using pointer           = typename std::iterator_traits<It>::pointer;
     using iterator_category = typename std::iterator_traits<It>::iterator_category;
 
-    static_assert(std::is_same<reference, const value_type&>::value, "Types are not the same");
-
-    BOOST_CONCEPT_USAGE(MyForwardIterator) {
-        It ret1 = i++;
-        reference ret2 = *i++;
+    BOOST_CONCEPT_USAGE(MyOutputIterator) {
+        *r = o;
+        It& res1 = ++r;
+        auto res2 = r++;
+        static_assert(std::is_convertible<decltype(res2), const It&>::value, "Can't be converted");
+        *r++ = o;
     }
 
 private:
-    It i;
+    It r;
+    ValueT o;
 };
+namespace internal {
+    template <class It>
+    struct MutableRef_t {
+        using value_type        = typename std::iterator_traits<It>::value_type;
+        using reference         = typename std::iterator_traits<It>::reference;
+
+        static_assert(std::is_same<reference, value_type&>::value, "Reference type is not mutable");
+    };
+
+    template <class It>
+    struct ConstRef_t {
+        using value_type        = typename std::iterator_traits<It>::value_type;
+        using reference         = typename std::iterator_traits<It>::reference;
+
+        static_assert(std::is_same<reference, const value_type&>::value, "Reference type is not constant");
+    };
+
+    template <class It, class = std::enable_if_t<std::is_default_constructible<It>::value>>
+    struct __MyForwardIteratorChecker : public MyInputIterator<It> {
+        using value_type        = typename std::iterator_traits<It>::value_type;
+        using difference_type   = typename std::iterator_traits<It>::difference_type;
+        using reference         = typename std::iterator_traits<It>::reference;
+        using pointer           = typename std::iterator_traits<It>::pointer;
+        using iterator_category = typename std::iterator_traits<It>::iterator_category;
+
+        BOOST_CONCEPT_USAGE(__MyForwardIteratorChecker) {
+            It ret1 = i++;
+            reference ret2 = *i++;
+        }
+
+    private:
+        It i;
+    };
+
+    template <class It>
+    struct __MyBidirectionalIteratorChecker : public __MyForwardIteratorChecker<It> {
+        using value_type        = typename std::iterator_traits<It>::value_type;
+        using difference_type   = typename std::iterator_traits<It>::difference_type;
+        using reference         = typename std::iterator_traits<It>::reference;
+        using pointer           = typename std::iterator_traits<It>::pointer;
+        using iterator_category = typename std::iterator_traits<It>::iterator_category;
+
+        BOOST_CONCEPT_USAGE(__MyBidirectionalIteratorChecker) {
+            It& ret1 = --a;
+            auto ret2 = a--;
+            static_assert(std::is_convertible<decltype(ret2), const It&>::value, "Can't be converted");
+
+            reference ret3 = *a--;
+        }
+
+    private:
+        It a;
+    };
+
+    template <class It>
+    struct __MyRandomAccessIteratorChecker : public __MyBidirectionalIteratorChecker<It> {
+        using value_type        = typename std::iterator_traits<It>::value_type;
+        using difference_type   = typename std::iterator_traits<It>::difference_type;
+        using reference         = typename std::iterator_traits<It>::reference;
+        using pointer           = typename std::iterator_traits<It>::pointer;
+        using iterator_category = typename std::iterator_traits<It>::iterator_category;
+
+        BOOST_CONCEPT_USAGE(__MyRandomAccessIteratorChecker) {
+            It& ret1 = i += n;
+            It ret2 = a + n;
+            It ret3 = n + a;
+            It& ret4 = r -= n;
+            It ret5 = i - n;
+            difference_type ret6 = b - a;
+            reference ret7 = i[n];
+
+            bool ret8 = a < b;
+            bool ret9 = a > b;
+            bool ret10 = a >= b;
+            bool ret11 = a <= b;
+        }
+
+    private:
+        It i;
+        const It a, b;
+        It& r;
+        difference_type n;
+    };
+}
+
 
 template <class It>
-struct MyBidirectionalIterator : public MyForwardIterator<It> {
-    using value_type        = typename std::iterator_traits<It>::value_type;
-    using difference_type   = typename std::iterator_traits<It>::difference_type;
-    using reference         = typename std::iterator_traits<It>::reference;
-    using pointer           = typename std::iterator_traits<It>::pointer;
-    using iterator_category = typename std::iterator_traits<It>::iterator_category;
-
-    BOOST_CONCEPT_USAGE(MyBidirectionalIterator) {
-        It& ret1 = --a;
-        auto ret2 = a--;
-        static_assert(std::is_convertible<decltype(ret2), const It&>::value, "Can't be converted");
-
-        reference ret3 = *a--;
-    }
-
-private:
-    It a;
-};
+struct MyForwardIterator : public internal::__MyForwardIteratorChecker<It>,
+                           public internal::ConstRef_t<It>
+{ };
 
 template <class It>
-struct MyRandomAccessIterator : public MyBidirectionalIterator<It> {
-    using value_type        = typename std::iterator_traits<It>::value_type;
-    using difference_type   = typename std::iterator_traits<It>::difference_type;
-    using reference         = typename std::iterator_traits<It>::reference;
-    using pointer           = typename std::iterator_traits<It>::pointer;
-    using iterator_category = typename std::iterator_traits<It>::iterator_category;
+struct MyMutableForwardIterator : public internal::__MyForwardIteratorChecker<It>,
+                                  public MyOutputIterator<It, typename internal::__MyForwardIteratorChecker<It>::value_type>,
+                                  public internal::MutableRef_t<It>
+{ };
 
-    BOOST_CONCEPT_USAGE(MyRandomAccessIterator) {
-        It& ret1 = i += n;
-        It ret2 = a + n;
-        It ret3 = n + a;
-        It& ret4 = r -= n;
-        It ret5 = i - n;
-        difference_type ret6 = b - a;
-        auto ret7 = i[n];
-        static_assert(std::is_convertible<decltype(ret7), reference>::value, "Can't be converted");
+template <class It>
+struct MyBidirectionalIterator : public internal::__MyBidirectionalIteratorChecker<It>,
+                                 public internal::ConstRef_t<It>
+{ };
 
-        bool ret8 = a < b;
-        bool ret9 = a > b;
-        bool ret10 = a >= b;
-        bool ret11 = a <= b;
-    }
+template <class It>
+struct MyMutableBidirectionalIterator : public internal::__MyBidirectionalIteratorChecker<It>,
+                                        public MyOutputIterator<It, typename  internal::__MyBidirectionalIteratorChecker<It>::value_type>,
+                                        public internal::MutableRef_t<It>
+{ };
 
-private:
-    It i;
-    const It a, b;
-    It& r;
-    difference_type n;
-};
+
+template <class It>
+struct MyRandomAccessIterator : public internal::__MyRandomAccessIteratorChecker<It>,
+                                public internal::ConstRef_t<It>
+{ };
+
+template <class It>
+struct MyMutableRandomAccessIterator : public internal::__MyRandomAccessIteratorChecker<It>,
+                                       public MyOutputIterator<It, typename  internal::__MyRandomAccessIteratorChecker<It>::value_type>,
+                                       public internal::MutableRef_t<It>
+{ };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include "RandAccessIterConcept.h"
+#include <iterator>
+#include <forward_list>
+#include <list>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //типа тесты. Ничего лучше не придумал...
+    BOOST_CONCEPT_ASSERT((MyInputIterator<std::istreambuf_iterator<char>>));
+    BOOST_CONCEPT_ASSERT((MyOutputIterator<std::ostreambuf_iterator<char>, char>));
+
+    BOOST_CONCEPT_ASSERT((MyForwardIterator<std::forward_list<int>::const_iterator>));
+    BOOST_CONCEPT_ASSERT((MyMutableForwardIterator<std::forward_list<int>::iterator>));
+
+    BOOST_CONCEPT_ASSERT((MyBidirectionalIterator<std::list<int>::const_iterator>));
+    BOOST_CONCEPT_ASSERT((MyMutableBidirectionalIterator<std::list<int>::iterator>));
+
+    BOOST_CONCEPT_ASSERT((MyRandomAccessIterator<std::vector<int>::const_iterator>));
+    BOOST_CONCEPT_ASSERT((MyMutableRandomAccessIterator<std::vector<int>::iterator>));
+
+    //негативные тесты
+    //для проверки раскомментировать соответствующую строчку
+    //BOOST_CONCEPT_ASSERT((MyOutputIterator<std::istreambuf_iterator<char>, char>));
+    //BOOST_CONCEPT_ASSERT((MyInputIterator<std::ostreambuf_iterator<char>>));
+
+    //BOOST_CONCEPT_ASSERT((MyBidirectionalIterator<std::forward_list<int>::const_iterator>));
+    //BOOST_CONCEPT_ASSERT((MyMutableBidirectionalIterator<std::forward_list<int>::iterator>));
+
+    //BOOST_CONCEPT_ASSERT((MyRandomAccessIterator<std::list<int>::const_iterator>));
+    //BOOST_CONCEPT_ASSERT((MyMutableRandomAccessIterator<std::list<int>::iterator>));
+
     return 0;
 }
